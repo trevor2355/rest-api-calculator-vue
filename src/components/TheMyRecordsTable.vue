71550ea33a8e23ddfac119505f31bc7a7777b1cb
Trevor Bell <trevor2355@gmail.com>
2020-06-12 15:38:49 -0600

    create a filter feature for user records

--- src/components/TheMyRecordsTable.vue
+++ src/components/TheMyRecordsTable.vue
@@ -1,43 +1,30 @@
 <template>
   <b-container>
-    <b-table id='my-records-table' striped hover :items="items" :fields='fields' :current-page="currentPage" :per-page="perPage">
+    <b-table id='my-records-table' striped hover :items="items" :fields='fields' :current-page="currentPage" :per-page="perPage" :filter='filter' :filterIncludedFields='filterIncludedFields' @filtered='onFiltered'>
     </b-table>
   </b-container>
 </template>
 <script>
-import moment from 'moment';
+// import moment from 'moment';
 export default {
   data() {
     return {
       items: this.records,
-      fields: [
-        { key: 'id', label: 'ID', sortable: true, sortDirection: 'desc' },
-        { key: 'uuid', label: 'UUID', sortable: true, class: 'text-center' },
-        { key: 'service_id', label: 'Service ID', sortable: true,
-          // sortByFormatted: true,
-          // filterByFormatted: true
-        },
-        { key: 'user_id', label: 'User ID', sortable: true, sortDirection: 'desc' },
-        { key: 'cost', label: 'Cost', sortable: true, sortDirection: 'desc' },
-        { key: 'user_balance', label: 'User Balance', sortable: true, sortDirection: 'desc' },
-        { key: 'service_response', label: 'Service Response', sortable: true, sortDirection: 'desc' },
-        { 
-          key: 'date', 
-          label: 'Date', 
-          sortable: true, 
-          sortDirection: 'desc',
-          formatter: (value) => {
-          return moment(value).format("dddd, MMMM Do YYYY, h:mm:ss")
-          }
-        }
-      ]
     };
   },
   props: [
     'records',
+    'fields',
     'perPage',
-    'currentPage'
-  ]
+    'currentPage',
+    'filter',
+    'filterIncludedFields'
+  ],
+  methods: {
+    onFiltered(filteredItems) {
+      this.$emit('filtered', filteredItems)
+    }
+  }
 };
 </script>
 <style>
--- src/views/userViews/MyRecords.vue
+++ src/views/userViews/MyRecords.vue
@@ -1,31 +1,65 @@
 <template>
   <div>
     <h1>My Records</h1>
-    <div class='select'>
-      <b-form-group
-        label="Per page"
-        label-size="sm"
-        label-align-sm="right"
-        label-cols-sm="6"
-        label-cols-md="4"
-        label-cols-lg="3"
-        label-for="perPageSelect"
-      >
-        <b-form-select
-          v-model="perPage"
-          id="perPageSelect"
-          :options="pageOptions"
-        ></b-form-select>
-      </b-form-group>
-    </div>
-    <b-pagination
-      v-model="currentPage"
-      :total-rows="totalRows"
-      :per-page="perPage"
-      aria-controls="my-records-table" 
-      align="fill"
-    ></b-pagination>
-    <TheMyRecordsTable :records='records' :key='key' :currentPage="currentPage" :perPage="perPage"></TheMyRecordsTable>
+      <div class='flex-container'>
+        <div class='flex-item'>
+          <b-form-group
+            label="Filter"
+            label-cols-sm="1"
+            label-align-sm="right"
+            label-size="sm"
+            label-for="filterInput"
+            class="mb-0"
+          >
+            <b-input-group size="sm">
+              <b-form-input
+                v-model="filter"
+                type="search"
+                id="filterInput"
+                placeholder="Type to Search"
+              ></b-form-input>
+            </b-input-group>
+          </b-form-group>
+          <b-form-group
+            label="Filter On"
+            label-cols-sm="1"
+            label-align-sm="right"
+            label-size="sm"
+            description="Leave all unchecked to filter on all data"
+            class="mb-0">
+            <b-form-checkbox-group v-model="filterOn" class="mt-1">
+              <b-form-checkbox v-for='field in fields' :key='field.key' :value='field.key'>{{ field.label }}</b-form-checkbox>
+            </b-form-checkbox-group>
+          </b-form-group>
+        </div>
+        <div class='flex-item'>
+          <div class='select'>
+            <b-form-group
+              label="Per page"
+              label-size="sm"
+              label-align-sm="right"
+              label-cols-sm="6"
+              label-cols-md="4"
+              label-cols-lg="3"
+              label-for="perPageSelect"
+            >
+              <b-form-select
+                v-model="perPage"
+                id="perPageSelect"
+                :options="pageOptions"
+              ></b-form-select>
+            </b-form-group>
+          </div>
+          <b-pagination
+            v-model="currentPage"
+            :total-rows="totalRows"
+            :per-page="perPage"
+            aria-controls="my-records-table" 
+            align="fill"
+          ></b-pagination>
+        </div>
+      </div>
+    <TheMyRecordsTable :records='records' :key='key' :currentPage="currentPage" :perPage="perPage" :filter="filter" :filterIncludedFields="filterOn" @filtered="onFiltered" :fields='fields'></TheMyRecordsTable>
   </div>
 </template>
 <script>
@@ -41,6 +75,31 @@ export default {
       currentPage: 1,
       perPage: 5,
       pageOptions: [5, 10, 15],
+      filter: null,
+      filterOn: [],
+      fields: [
+        { key: 'id', label: 'ID', sortable: true, sortDirection: 'desc' },
+        { key: 'uuid', label: 'UUID', sortable: true, class: 'text-center' },
+        { key: 'service_id', label: 'Service ID', sortable: true,
+          // sortByFormatted: true,
+          // filterByFormatted: true
+        },
+        { key: 'user_id', label: 'User ID', sortable: true, sortDirection: 'desc' },
+        { key: 'cost', label: 'Cost', sortable: true, sortDirection: 'desc' },
+        { key: 'user_balance', label: 'User Balance', sortable: true, sortDirection: 'desc' },
+        { key: 'service_response', label: 'Service Response', sortable: true, sortDirection: 'desc' },
+        { 
+          key: 'date', 
+          label: 'Date', 
+          sortable: true, 
+          sortDirection: 'desc',
+          formatter: (value) => {
+          return moment(value).format("dddd, MMMM Do YYYY, h:mm:ss")
+          },
+          sortByFormatted: true,
+          filterByFormatted: true
+        }
+      ]
   }
   },
   props: {
@@ -80,6 +139,12 @@ export default {
           console.log(err)
         })
     },
+    onFiltered(filteredItems) {
+      // Trigger pagination to update the number of buttons/pages due to filtering
+      console.log('filteredItems: ', filteredItems)
+      this.totalRows = filteredItems.length
+      this.currentPage = 1
+    }
     // convertServiceIdToString(serviceId) {
     //   let matchedService = this.services.filter(service => service.id === serviceId);
     //   return matchedService.type
@@ -108,10 +173,10 @@ export default {
 h1 {
   padding: 8px 0
 }
-.select {
-  padding: 0 40%
-}
-.pagination {
-  padding: 0 30%
+.flex-container {
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  column-gap: 80px;
+  margin: 0 88px;
 }
 </style>
