854ba7b2083b478feefbb4c0f0630076fd07eea8
Trevor Bell <trevor2355@gmail.com>
2020-06-24 22:07:55 -0600

    make necessary changes so all service logic happens on back-end

--- src/views/userViews/Services.vue
+++ src/views/userViews/Services.vue
@@ -22,7 +22,11 @@
           name="additionVal2"
           class="input"
         />
-        <b-button @click="add" :disabled="additionSubtractionDisabled"
+        <b-button
+          id="1"
+          name="addition"
+          @click="requestService"
+          :disabled="additionDisabled"
           >Request Service</b-button
         >
         <span v-if="additionResult || additionResult === 0"
@@ -47,7 +51,11 @@
           name="subtractionVal2"
           class="input"
         />
-        <b-button @click="subtract" :disabled="additionSubtractionDisabled"
+        <b-button
+          id="2"
+          name="subtraction"
+          @click="requestService"
+          :disabled="subtractionDisabled"
           >Request Service</b-button
         >
         <span v-if="subtractionResult || subtractionResult === 0"
@@ -72,7 +80,11 @@
           name="multiplicationVal2"
           class="input"
         />
-        <b-button @click="multiply" :disabled="multiplicationDivisionDisabled"
+        <b-button
+          id="3"
+          name="multiplication"
+          @click="requestService"
+          :disabled="multiplicationDisabled"
           >Request Service</b-button
         >
         <span v-if="multiplicationResult || multiplicationResult === 0"
@@ -97,7 +109,11 @@
           name="divisionVal2"
           class="input"
         />
-        <b-button @click="divide" :disabled="multiplicationDivisionDisabled"
+        <b-button
+          id="4"
+          name="division"
+          @click="requestService"
+          :disabled="divisionDisabled"
           >Request Service</b-button
         >
         <span v-if="divisionResult || divisionResult === 0"
@@ -114,31 +130,37 @@
           name="squareRootVal"
           class="input"
         />
-        <b-button @click="squareRoot" :disabled="squareRootDisabled"
+        <b-button
+          id="5"
+          name="squareRoot"
+          @click="requestService"
+          :disabled="squareRootDisabled"
           >Request Service</b-button
         >
-        <span v-if="squareRootResult || squareRootResult === 0">{{
-          squareRootResult
-        }}</span>
+        <span v-if="squareRootResult || squareRootResult === 0"
+          >= {{ squareRootResult }}</span
+        >
       </div>
       <div>
         <h3>Random String</h3>
         <p>Cost: {{ "random_string" | findCostOfService(services) }}</p>
         <label for="val1">Length (1-32)</label>
         <input
-          v-model="generateRandomStringVal"
+          v-model="randomStringGeneratorVal"
           type="number"
-          name="generateRandomStringVal"
+          name="randomStringGeneratorVal"
           class="input"
         />
         <b-button
-          @click="generateRandomString"
+          id="6"
+          name="randomStringGenerator"
+          @click="requestService"
           :disabled="randomStringGeneratorDisabled"
           >Request Service</b-button
         >
-        <span v-if="randomStringGeneratorResult">{{
-          randomStringGeneratorResult
-        }}</span>
+        <span v-if="randomStringGeneratorResult"
+          >= {{ randomStringGeneratorResult }}</span
+        >
       </div>
     </div>
   </div>
@@ -159,7 +181,7 @@ export default {
       divisionVal1: null,
       divisionVal2: null,
       squareRootVal: null,
-      generateRandomStringVal: null,
+      randomStringGeneratorVal: null,
       additionResult: null,
       subtractionResult: null,
       multiplicationResult: null,
@@ -169,17 +191,65 @@ export default {
     };
   },
   computed: {
-    additionSubtractionDisabled: function() {
-      return this.userBalance < 2;
+    additionDisabled: function() {
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "addition"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
     },
-    multiplicationDivisionDisabled: function() {
-      return this.userBalance < 4;
+    subtractionDisabled: function() {
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "subtraction"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
+    },
+    multiplicationDisabled: function() {
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "multiplication"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
+    },
+    divisionDisabled: function() {
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "division"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
     },
     squareRootDisabled: function() {
-      return this.userBalance < 5;
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "square_root"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
     },
     randomStringGeneratorDisabled: function() {
-      return this.userBalance < 10;
+      if (this.services) {
+        let service = this.services.find(
+          element => element.type === "random_string"
+        );
+        return this.userBalance < service.cost;
+      } else {
+        return true;
+      }
     }
   },
   created() {
@@ -206,22 +276,6 @@ export default {
           console.log(err);
         });
     },
-    async updateUser(updateObject) {
-      let options = {
-        method: "PUT",
-        headers: {
-          "content-type": "application/json",
-          Authorization: JSON.parse(localStorage.getItem("JSWT"))
-        },
-        body: JSON.stringify(updateObject)
-      };
-      let response = await fetch(
-        `${this.$hostname}/api/users/${store.user.id}`,
-        options
-      );
-      let data = await response.json();
-      return data;
-    },
     async getUser() {
       let options = {
         method: "GET",
@@ -237,113 +291,44 @@ export default {
       let user = await response.json();
       return user[0];
     },
-    async postRecord(record) {
+    requestService(event) {
+      let serviceId = event.target.id;
+      let serviceType = event.target.name;
+      let requestedServiceVals;
+      if (serviceId <= 4) {
+        requestedServiceVals = {
+          [serviceType + "Val1"]: this[serviceType + "Val1"],
+          [serviceType + "Val2"]: this[serviceType + "Val2"]
+        };
+      } else {
+        requestedServiceVals = {
+          [serviceType + "Val"]: this[serviceType + "Val"]
+        };
+      }
       let options = {
         method: "POST",
         headers: {
           "content-type": "application/json",
           Authorization: JSON.parse(localStorage.getItem("JSWT"))
         },
-        body: JSON.stringify(record)
+        body: JSON.stringify(requestedServiceVals)
       };
-      let response = await fetch(`${this.$hostname}/api/records`, options);
-      let insertedRecord = await response.json();
-      return insertedRecord;
-    },
-    add() {
-      this.additionResult =
-        parseFloat(this.additionVal1) + parseFloat(this.additionVal2);
-      this.handleServiceRequest("addition", this.additionResult);
-    },
-    subtract() {
-      this.subtractionResult =
-        parseFloat(this.subtractionVal1) - parseFloat(this.subtractionVal2);
-      this.handleServiceRequest("subtraction", this.subtractionResult);
-    },
-    multiply() {
-      this.multiplicationResult =
-        parseFloat(this.multiplicationVal1) *
-        parseFloat(this.multiplicationVal2);
-      this.handleServiceRequest("multiplication", this.multiplicationResult);
-    },
-    divide() {
-      this.divisionResult =
-        parseFloat(this.divisionVal1) / parseFloat(this.divisionVal2);
-      this.handleServiceRequest("division", this.divisionResult);
-    },
-    squareRoot() {
-      this.squareRootResult = Math.sqrt(parseFloat(this.squareRootVal));
-      this.handleServiceRequest("square_root", this.squareRootResult);
-    },
-    generateRandomString() {
-      let length = this.generateRandomStringVal;
-      let options = {
-        method: "POST",
-        headers: {
-          "content-type": "application/json",
-          Authorization: JSON.parse(localStorage.getItem("JSWT"))
-        },
-        body: JSON.stringify({ length })
-      };
-      fetch(`${this.$hostname}/api/services/randomstringgenerator`, options)
+      fetch(`${this.$hostname}/api/services/request/${serviceId}`, options)
         .then(response => {
-          if (response.status !== 200) {
-            throw new Error("Length not within valid range");
-          }
           return response.json();
         })
-        .then(string => {
-          this.randomStringGeneratorResult = string.string;
-          this.handleServiceRequest(
-            "random_string",
-            this.randomStringGeneratorResult
-          );
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    },
-    handleServiceRequest(serviceType, serviceResult) {
-      let serviceInfo = this.findCostAndId(serviceType);
-      let newBalance = store.user.balance - serviceInfo.cost;
-      let record = {
-        service_id: serviceInfo.id,
-        cost: serviceInfo.cost,
-        user_balance: newBalance,
-        service_response: serviceResult,
-        date: new Date(),
-        user_id: store.user.id
-      };
-      this.updateUser({ balance: newBalance })
-        .then(response => {
-          response;
-          this.userBalance = newBalance;
+        .then(result => {
+          this[serviceType + "Result"] = result.result;
           return this.getUser();
         })
         .then(user => {
+          this.userBalance = user.balance;
           store.user = user;
           localStorage.setItem("user", JSON.stringify(user));
-          return this.postRecord(record);
-        })
-        .then(record => {
-          return record;
         })
         .catch(err => {
           console.log(err);
         });
-    },
-    findCostAndId(serviceName) {
-      if (this.services) {
-        let matchedService = this.services.filter(
-          service => service.type === serviceName
-        );
-        return {
-          cost: matchedService[0].cost,
-          id: matchedService[0].id
-        };
-      } else {
-        return "cost not available";
-      }
     }
   },
   filters: {
